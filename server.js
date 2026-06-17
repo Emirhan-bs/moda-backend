59a8c038bb8771e373aaeeb073fa9cc126af85b9
Emirhan Buyuksenirli <emirhanbuyuksenirli@gmail.com>
2026-06-17 13:51:55 +0300

    translate fix

--- server.js
+++ server.js
@@ -28,3 +28,18 @@ app.listen(PORT, ()=>{
     console.log(`Server ${PORT} portunda çalışıyor`);
     
 })
+
+app.post('/api/translate', async (req, res) => {
+  const { text, targetLang } = req.body;
+  
+  try {
+    const response = await fetch(
+      `https://translate.googleapis.com/translate_a/single?client=gtx&sl=tr&tl=${targetLang}&dt=t&q=${encodeURIComponent(text)}`
+    );
+    const data = await response.json();
+    const translated = data[0].map((item) => item[0]).join('');
+    res.json({ translated });
+  } catch (error) {
+    res.status(500).json({ error: 'Çeviri başarısız' });
+  }
+});
